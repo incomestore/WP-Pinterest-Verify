5905f9976e824b77ca600074761ed99a260bba98
Nick Young <mycorpweb@gmail.com>
2014-10-26 16:14:57 -0600

    Update one more string for i18n

--- pinterest-verify/views/admin-install-notice.php
+++ pinterest-verify/views/admin-install-notice.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div id="pvr-install-notice" class="updated">
 	<p>
-		<?php echo $this->get_plugin_title() . __( ' is now installed.', 'pvr' ); ?>
+		<?php printf( __( '%1$s is now installed.', 'pvr' ), $this->get_plugin_title() ); ?>
 		<a href="<?php echo add_query_arg( 'page', $this->plugin_slug, admin_url( 'admin.php' ) ); ?>" class="button-primary"><?php _e( 'Setup Pinterest Verification Now', 'pvr' ); ?></a>
 		<a href="<?php echo add_query_arg( 'pvr-dismiss-install-nag', 1 ); ?>" class="button-secondary"><?php _e( 'Hide this', 'pvr' ); ?></a>
 	</p>
